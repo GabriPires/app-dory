a25366de176e2929a62f97bfbabfc3d8d8d659e7
Gabriel Pires <gabpiresdev@gmail.com>
2021-12-09 21:21:19 -0300

    Dados sobre desaparecido vindos da API

---
+++ src/components/MoreInfoText/index.ts
@@ -0,0 +1,5 @@
+import styled from 'styled-components/native';
+
+export const MoreInfoText = styled.Text`
+  max-width: 130px;
+`;
--- src/pages/MissingDetailsScreen.tsx
+++ src/pages/MissingDetailsScreen.tsx
@@ -14,11 +14,42 @@ import { InformationTitle } from '../components/InformationContainer/style';
 import { Divider } from '../components/Divider';
 import { ScrollView } from 'react-native-gesture-handler';
 import api from '../services/api';
-import { MissingPeopleProps } from './MissingScreen';
+import { MoreInfoText } from '../components/MoreInfoText';
 
 interface Params {
   id: string;
 }
+
+export type MissingPeopleProps = {
+  desaparecido: {
+    codigo: number;
+    encontrado: string;
+    idade: number;
+    pessoa: {
+      codigo: number;
+      cpf: string;
+      rg: string;
+      nome: string;
+      nascimento: string;
+      sexo: string;
+      cutis: string;
+      tipo: string | undefined;
+    };
+  };
+  maisInfos: {
+    cabelo: string;
+    olhos: string;
+    altura: string;
+    peso: string;
+    descricao: string;
+    tipoSanguineo: string;
+    deficienciaMental: string;
+    deficienciaFisica: string;
+    restricaoAlimentar: string;
+    restricaoMedicamentos: string;
+    doencas: string;
+  };
+};
 
 export const MissingDetailsScreen: React.FC = () => {
   const route = useRoute();
@@ -41,14 +72,12 @@ export const MissingDetailsScreen: React.FC = () => {
         }}
         style={{ marginTop: 16 }}
       />
-      <Title>{missing?.pessoa.nome}</Title>
-      <Text style={{ textAlign: 'center', marginBottom: 24 }}>00 anos</Text>
+      <Title>{missing?.desaparecido.pessoa.nome ?? 'Nome'}</Title>
+      <Text style={{ textAlign: 'center', marginBottom: 24 }}>
+        {missing?.desaparecido.idade} anos
+      </Text>
       <Title>Sobre esta pessoa</Title>
-      <Description>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto fugit
-        ratione voluptates commodi nemo, nobis ipsa quisquam ea at enim animi
-        eveniet voluptatum ipsum veritatis libero ad sunt maxime incidunt!
-      </Description>
+      <Description>{missing?.maisInfos.descricao}</Description>
       <View style={{ width: '60%', alignSelf: 'center' }}>
         <Button title={'Eu vi esta pessoa'} onPress={() => console.log('oi')} />
       </View>
@@ -56,29 +85,29 @@ export const MissingDetailsScreen: React.FC = () => {
         <InformationDetails>
           <View>
             <InformationTitle>Desaparecido desde</InformationTitle>
-            <Text>22/12/2020</Text>
+            <MoreInfoText>22/12/2020</MoreInfoText>
           </View>
           <View>
             <InformationTitle>Etnia</InformationTitle>
-            <Text>{missing?.pessoa.cutis}</Text>
+            <MoreInfoText>{missing?.desaparecido.pessoa.cutis}</MoreInfoText>
           </View>
           <View>
             <InformationTitle>Cor dos olhos</InformationTitle>
-            <Text>Castanhos</Text>
+            <MoreInfoText>{missing?.maisInfos.olhos}</MoreInfoText>
           </View>
         </InformationDetails>
         <InformationDetails>
           <View>
             <InformationTitle>Sexo</InformationTitle>
-            <Text>{missing?.pessoa.sexo}</Text>
+            <MoreInfoText>{missing?.desaparecido.pessoa.sexo}</MoreInfoText>
           </View>
           <View>
             <InformationTitle>Tipo sanguíneo</InformationTitle>
-            <Text>A+</Text>
+            <MoreInfoText>A+</MoreInfoText>
           </View>
           <View>
             <InformationTitle>Cor do cabelo</InformationTitle>
-            <Text>Moreno</Text>
+            <MoreInfoText>{missing?.maisInfos.cabelo}</MoreInfoText>
           </View>
         </InformationDetails>
       </InformationContainer>
@@ -88,21 +117,23 @@ export const MissingDetailsScreen: React.FC = () => {
         <InformationDetails>
           <View>
             <InformationTitle>Deficiência mental</InformationTitle>
-            <Text>Não possui</Text>
+            <MoreInfoText>{missing?.maisInfos.deficienciaMental}</MoreInfoText>
           </View>
           <View>
             <InformationTitle>Comidas alergênicas</InformationTitle>
-            <Text>Não possui</Text>
+            <MoreInfoText>{missing?.maisInfos.restricaoAlimentar}</MoreInfoText>
           </View>
         </InformationDetails>
         <InformationDetails>
           <View>
             <InformationTitle>Deficiência física</InformationTitle>
-            <Text>Não possui</Text>
+            <MoreInfoText>{missing?.maisInfos.deficienciaFisica}</MoreInfoText>
           </View>
           <View>
             <InformationTitle>Medicamentos alergênicos</InformationTitle>
-            <Text>Ibuprofeno</Text>
+            <MoreInfoText>
+              {missing?.maisInfos.restricaoMedicamentos}
+            </MoreInfoText>
           </View>
         </InformationDetails>
       </InformationContainer>
