735aa430aeb5be0db9d3f8834cff02a23d477343
Gabriel Pires <gabpiresdev@gmail.com>
2021-12-10 22:00:58 -0300

    Alterações na home

--- src/components/Title/index.ts
+++ src/components/Title/index.ts
@@ -6,3 +6,10 @@ export const Title = styled.Text`
   font-size: 16px;
   margin-top: 8px;
 `;
+
+export const TitleUppercase = styled.Text`
+  font-weight: bold;
+  color: #293845;
+  margin-bottom: 16px;
+  text-transform: uppercase;
+`;
--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
-import { View } from 'react-native';
+import { ScrollView, View } from 'react-native';
+import { Description } from '../components/Description';
 import { ReconnectedPeopleCounter } from '../components/ReconnectedPeopleCounter';
+import { TitleUppercase } from '../components/Title';
 
 export const HomeScreen: React.FC = () => {
   return (
-    <View>
+    <ScrollView>
       <ReconnectedPeopleCounter counter={53} />
-    </View>
+      <View
+        style={{
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <TitleUppercase>Sobre o projeto</TitleUppercase>
+
+        <Description>
+          O projeto Dory proporciona ao usuário um sistema web para cadastrar
+          pessoas que desapareceram ou que possam vir a desaparecer, com o
+          objetivo de mediar a relação entre as famílias e terceiros que possam
+          encontrá-las.
+        </Description>
+        <Description>
+          O conceito principal da aplicação é fornecer uma busca prática com
+          dados centralizados, e uma plataforma intuitiva para que o processo
+          seja feito de maneira rápida, segura e eficiente. Além claro, de um
+          serviço de informações didáticas para ocasiões inesperadas.
+        </Description>
+      </View>
+    </ScrollView>
   );
 };
